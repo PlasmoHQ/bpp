6390746d95ceb22506f76ee874fb626e4bb292c2
Louis ❤☮r <6723574+louisgv@users.noreply.github.com>
2022-02-10 02:57:10 -0400

    Use ubuntu to build

--- dist/index.js
+++ dist/index.js
@@ -53402,7 +53402,6 @@ exports.getSignInCookie = getSignInCookie;
 
 "use strict";
 
-
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
